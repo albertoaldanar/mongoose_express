79e4e04f5b36645dc917ab30bd83d1c007704ec2
Alberto Aldana <albertoaldanar@hotmail.com>
2018-07-07 13:52:44 -0600

    put methos ready

--- routes/routes.js
+++ routes/routes.js
@@ -8,12 +8,30 @@ router.get("/products", function (req, res){
   })
 });
 
-//Busqueda de porducto por id de codigo QR
-router.get("/products/:id", function (req, res){
-  Product.findOne({p_id: req.params.id}, function(err, object){
-    res.json({product: object})
-  })
-});
+//Rutas que necesitan tener acceso al id del producto
+
+//Ruta que es llamada por el id del QR
+  router.get("/products/:id", function (req, res){
+    Product.findOne({p_id: req.params.id}, function(err, object){
+      res.json({product: object})
+    })
+  });
+
+//Ruata para mandar un producto al carrito
+  router.put("/products/:id", function (req, res){
+    Product.findOneAndUpdate({p_id: req.params.id}, function(err, object){
+      if(err)
+        res.send(err);
+
+      object.inCart = req.body.inCart;
+
+      object.save(function(err){
+        if(err)
+          res.send(err)
+        res.json({message: "Product updated"});
+      })
+    })
+  });
 
 //Ruta para mostrar los prductos en el carrito
 router.get("/cart", function(req, res){
@@ -21,5 +39,7 @@ router.get("/cart", function(req, res){
     res.json({cart: cart})
   })
 })
+
+//Ruta para modificar el atributo inCart a true es decir meter un producto al carrito
 
 module.exports = router;
